4cc3de7381cc1b23864aa351ad809a1e39b5f428
RicardojunioSM <ricardo.m.gama@hotmail.com>
2021-06-09 00:55:11 -0300

    add Data-Struture

---
+++ Data-Struct/README.md
@@ -0,0 +1,1 @@
+teste para o readme.md
---
+++ Data-Struct/Stacks.c
@@ -0,0 +1,86 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+   
+struct stack{ 
+      int value; 
+      struct stack* node; 
+  }; 
+   
+typedef struct stack* No; 
+ 
+void push(No* SP, int num){ 
+    No newnod = malloc(sizeof(No)); 
+   
+    newnod->value = num; 
+    newnod->node = NULL; 
+     
+    if(*SP == NULL){ 
+      *SP = newnod; 
+      newnod = *SP; 
+    }else{ 
+        newnod->node = *SP; 
+        *SP = newnod; 
+    } 
+   
+  } 
+   
+  void pop(No* SP){ 
+    No aux = *SP; 
+     
+    if(*SP == NULL){ 
+      printf("      lista vazia..\n"); 
+    }else{ 
+      *SP = (*SP)->node; 
+      free(aux);  
+    } 
+  } 
+   
+  void printStack(No* SP){ 
+    No aux = *SP; 
+    while(aux != NULL){ 
+      printf("           %d\n",aux->value);  
+      aux = aux->node; 
+    } 
+    printf("\n"); 
+  } 
+   
+  void show(){ 
+    printf("Selecione sua opção \n(1) inserir intem da pilha \n(2) remover intem da pilha \n(3) sair\n\nDigite:"); 
+  } 
+   
+   
+  int main(int argc , char* argv[]){ 
+    No stack = NULL; 
+    int chose,num; 
+     
+   
+    show(); 
+    scanf("%d",&chose); 
+     
+    while(chose != 3){ 
+       
+      switch(chose){ 
+          case 1:  
+            printf("\nDigite um numero para colocar na pilha.."); 
+            scanf("%d",&num); 
+            push(&stack,num); 
+            printf("\n Inserindo intem da pilha\n"); 
+            printStack(&stack); 
+          break; 
+           
+          case 2: 
+            printf("\n Removendo inten da pilha \n"); 
+            pop(&stack); 
+            printStack(&stack); 
+          break; 
+          default: 
+            printf("\nEscolha invalida\n"); 
+            show(); 
+          break;  
+      } 
+      show(); 
+      scanf("%d",&chose); 
+    }
+    return 0; 
+  } 
+
